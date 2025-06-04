64c48ca4c670522f6d7161709577935b92fee5e7
mato533 <taiki.abe.00@gmail.com>
2025-06-05 04:37:29 +0900

    style: apply style
    [skip ci]

--- packages/vscode-webdriverio/tests/package.test.ts
+++ packages/vscode-webdriverio/tests/package.test.ts
@@ -3,8 +3,7 @@ import { describe, it, expect } from 'vitest'
 
 import pkg from '../package.json' with { type: 'json' }
 
-describe('package.json', ()=>{
-
+describe('package.json', () => {
     const testPattern = []
     for (const [key, value] of Object.entries(DEFAULT_CONFIG_VALUES)) {
         testPattern.push([key, value])
@@ -12,7 +11,7 @@ describe('package.json', ()=>{
 
     const pkgConfig = pkg.contributes.configuration.properties
 
-    it.each(testPattern)('should set default value - %s', (key, value)=>{
+    it.each(testPattern)('should set default value - %s', (key, value) => {
         // @ts-ignore
         const pkgConfigValue = pkgConfig[`${EXTENSION_ID}.${key}`]['default']
         expect(pkgConfigValue).toStrictEqual(value)
